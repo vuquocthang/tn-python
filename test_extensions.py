8803ef17ab2b7086b5df706f9e0b5c709d0f798d
vuquocthang <vuquocthang63@gmail.com>
2018-08-03 02:15:23 -0400

    Extensions

--- test_extensions.py
+++ test_extensions.py
@@ -1,3 +1,16 @@
 import helper
+from pyvirtualdisplay import Display
+from xvfbwrapper import Xvfb
+
+vdisplay = Xvfb()
+vdisplay.start()
+display = Display(visible=0, size=(800, 600))
+display.start()
 
 helper._init_with_extension()
+
+driver.save_screenshot("test-extensions.png")
+
+driver.quit()
+display.stop()
+vdisplay.stop()
