c6c8f2947bd18c6cdabcb03bdb13a72df67a948a
thang vu quoc <vuquocthang63@gmail.com>
2018-08-07 01:56:21 +0700

    post

--- post.py
+++ post.py
@@ -39,11 +39,11 @@ while True:
                     display.start()
 
                     # init driver
-
-
                     useragent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0"
                     driver = helper._init_with_useragent(clone['ip'], clone['port'], clone['c_user'], clone['xs'], useragent)
                     driver.get("https://facebook.com")
+
+                    driver.save_screenshot("init.png")
 
                     try:
                         # files
