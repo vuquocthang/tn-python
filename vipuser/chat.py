0e69658d8afe76678ac982a5de7e431073e2964f
thang vu quoc <vuquocthang63@gmail.com>
2018-09-13 06:23:16 +0700

    First commit

---
+++ vipuser/chat.py
@@ -0,0 +1,80 @@
+import helper
+import requests
+import threading
+import time
+import os
+#from xvfbwrapper import Xvfb
+#from pyvirtualdisplay import Display
+import myutil.init
+import myutil.log as mylog
+import vipuser.vip_user_helper as viphelper
+
+#url = "http://toolnuoi999.tk"
+url = "http://192.168.81.139:8001"
+
+logging_path = os.path.join( os.path.dirname(os.path.abspath(__file__)), 'image-chat-logging')
+
+NUM_WORKERS = 1
+
+def create_workers():
+    for x in range(NUM_WORKERS):
+        t = threading.Thread(target=work)
+        #t.daemon = True
+        t.start()
+
+def work():
+    while True:
+        try:
+            clone = requests.get("{}/api/clones/Vip/all".format(url), {
+                'api_key': helper.get_api_key()
+            }).json()
+
+            if clone is None:
+                break
+
+            mylog.save("chat", "{}".format(clone))
+
+            #vdisplay = Xvfb()
+            #vdisplay.start()
+            #display = Display(visible=0, size=(800, 600))
+            #display.start()
+
+            driver = None
+
+            try:
+                driver = myutil.init._init(clone['ip'], clone['port'], clone['c_user'], clone['xs'])
+
+                check = myutil.init._is_checkpoint(driver, clone)
+
+                if check is False:
+                    print("Clone is checkpoint")
+                    driver.quit()
+                    #break
+
+
+                viphelper.newest_message(driver, clone['user']['vip_keywords'])
+                viphelper.request_message(driver, clone['user']['vip_keywords'])
+
+                driver.save_screenshot(
+                    os.path.join(logging_path, 'chat-success-{}.{}'.format(clone['c_user'], 'png'))
+                )
+            except Exception as e:
+                print("Exception : {}".format(e))
+
+                driver.save_screenshot(
+                    os.path.join( logging_path , 'chat-exception-{}.{}'.format( clone['c_user'], 'png'))
+                )
+                driver.quit()
+            finally:
+                driver.quit()
+                print("Done")
+
+            #display.stop()
+            #vdisplay.stop()
+
+        except Exception as e:
+            print(e)
+
+        time.sleep(5)
+create_workers()
+
---
+++ vipuser/vip_user_helper.py
@@ -0,0 +1,88 @@
+import helper
+
+def get_message_from_keyword(keywords, message, name):
+    for k in keywords:
+        if k['key'].lower() in message.lower():
+            return k['value'].replace("[name]", name)
+
+    return False
+
+def newest_message(driver, keywords):
+    try:
+        driver.get("https://m.facebook.com/messages/?folder=unread")
+
+        conversations = driver.find_elements_by_xpath("//div[@id='root']//tbody//a[contains(@href,'/messages/read')]")
+
+        if (len(conversations) == 0):
+            return 0
+
+        links = []
+
+        for conversation in conversations:
+            link = conversation.get_attribute('href')
+            links.append(link)
+
+        for link in links:
+            try:
+                print(link)
+                driver.get(link)
+                recipient_message = driver.find_element_by_xpath("//div[@id='fua']//span[1]").text
+                recipient_name = driver.find_element_by_xpath("//div[@id='fua']//strong[1]").text
+
+                message = get_message_from_keyword(keywords, recipient_message, recipient_name)
+
+                #print(message)
+
+                if message is not False:
+                    print(message)
+                    helper.send_message(driver, link, message)
+                else:
+                    print("Message not has keyword")
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        return "error : {}".format(e)
+    else:
+        return True
+
+def request_message(driver, keywords):
+    try:
+        driver.get("https://m.facebook.com/messages/?folder=pending")
+
+        conversations = driver.find_elements_by_xpath("//div[@id='root']//tbody//a[contains(@href,'/messages/read')]")
+
+        if (len(conversations) == 0):
+            return 0
+
+        links = []
+
+        for conversation in conversations:
+            link = conversation.get_attribute('href')
+
+            links.append(link)
+
+        for link in links:
+            try:
+                print(link)
+                driver.get(link)
+                # send_message(driver, link, "This is a virus" )
+
+                recipient_message = driver.find_element_by_xpath("//div[@id='fua']//span[1]").text
+                recipient_name = driver.find_element_by_xpath("//div[@id='fua']//strong[1]").text
+
+                message = get_message_from_keyword(keywords, recipient_message, recipient_name)
+
+                #print(message)
+
+                if message is not False:
+                    helper.send_message(driver, link, message)
+                else:
+                    print("Message not has keyword")
+            except Exception as e:
+                print(e)
+
+    except Exception as e:
+        return "error : {}".format(e)
+    else:
+        return True
+
