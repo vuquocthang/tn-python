79f5afc9be7a7b73c1b579dfdc63dbe3f29a2c2a
thang vu quoc <vuquocthang63@gmail.com>
2018-09-13 10:44:59 +0700

    First commit

--- restartcore.py
+++ restartcore.py
@@ -10,10 +10,11 @@ subprocess.run(['supervisorctl', 'stop', 'all'], stdout=subprocess.PIPE)
 '''
 time.sleep(5)
 subprocess.run(['supervisorctl', 'restart', 'all'], stdout=subprocess.PIPE)
-
 '''
 
 time.sleep(5)
 subprocess.run(['supervisorctl', 'restart', 'post'], stdout=subprocess.PIPE)
 subprocess.run(['supervisorctl', 'restart', 'server'], stdout=subprocess.PIPE)
+subprocess.run(['supervisorctl', 'restart', 'vipchat'], stdout=subprocess.PIPE)
+subprocess.run(['supervisorctl', 'restart', 'vipcomment'], stdout=subprocess.PIPE)
 
